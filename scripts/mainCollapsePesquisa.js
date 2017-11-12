f71072a0279986f9e0aa84f41328d8bb21437bf7
rpao <rpao@cin.ufpe.br>
2017-11-12 07:27:23 -0300

    integraçao v1
    integraçado front-back

--- scripts/mainCollapsePesquisa.js
+++ scripts/mainCollapsePesquisa.js
@@ -15,8 +15,8 @@
  */
 'use strict';
 
-// Initializes FriendlyChat.
-function FriendlyChat() {
+// Initializes mainPesquisas.
+function mainPesquisas() {
   this.checkSetup();
 
   // Shortcuts to DOM Elements.
@@ -52,7 +52,7 @@ function FriendlyChat() {
 
 // Enables or disables the submit button depending on the values of the input
 // fields.
-FriendlyChat.prototype.toggleButton = function() {
+mainPesquisas.prototype.toggleButton = function() {
   if (this.messageInput.value) {
     this.submitButton.removeAttribute('disabled');
   } else {
@@ -60,8 +60,138 @@ FriendlyChat.prototype.toggleButton = function() {
   }
 };
 
+// Sets up shortcuts to Firebase features and initiate firebase auth.
+mainPesquisas.prototype.initFirebase = function() {
+  // Shortcuts to Firebase SDK features.
+  this.auth = firebase.auth();
+  this.database = firebase.database();
+  this.storage = firebase.storage();
+  // Initiates Firebase auth and listen to auth state changes.
+  this.auth.onAuthStateChanged(this.onAuthStateChanged.bind(this));
+};
+
+// Sets the URL of the given img element with the URL of the image stored in Cloud Storage.
+mainPesquisas.prototype.setImageUrl = function(imageUri, imgElement) {
+  // If the image is a Cloud Storage URI we fetch the URL.
+  if (imageUri.startsWith('gs://')) {
+    imgElement.src = mainPesquisas.LOADING_IMAGE_URL; // Display a loading image first.
+    this.storage.refFromURL(imageUri).getMetadata().then(function(metadata) {
+      imgElement.src = metadata.downloadURLs[0];
+    });
+  } else {
+    imgElement.src = imageUri;
+  }
+};
+
+// Signs-in Friendly Chat.
+mainPesquisas.prototype.signIn = function() {
+  // Sign in Firebase using popup auth and Google as the identity provider.
+  var provider = new firebase.auth.GoogleAuthProvider();
+  this.auth.signInWithPopup(provider);
+};
+
+// Signs-out of Friendly Chat.
+mainPesquisas.prototype.signOut = function() {
+  // Sign out of Firebase.
+  this.auth.signOut();
+};
+
+// Triggers when the auth state change for instance when the user signs-in or signs-out.
+mainPesquisas.prototype.onAuthStateChanged = function(user) {
+  if (user) { // User is signed in!
+    // Get profile pic and user's name from the Firebase user object.
+    var profilePicUrl = user.photoURL;
+    var userName = user.displayName;
+
+    // Show user's profile and sign-out button.
+    this.signOutButton.removeAttribute('hidden');
+
+    // Hide sign-in button.
+    this.signInButton.setAttribute('hidden', 'true');
+
+    // We load currently existing chant messages.
+    this.loadPesquisas();
+	
+    // We save the Firebase Messaging Device token and enable notifications.
+    this.saveMessagingDeviceToken();
+  } else { // User is signed out!
+    // Hide user's profile and sign-out button.
+    this.signOutButton.setAttribute('hidden', 'true');
+
+    // Show sign-in button.
+    this.signInButton.removeAttribute('hidden');
+  }
+};
+
+// Returns true if user is signed-in. Otherwise false and displays a message.
+mainPesquisas.prototype.checkSignedInWithMessage = function() {
+  // Return true if the user is signed in Firebase
+  if (this.auth.currentUser) {
+    return true;
+  }
+
+  // Display a message to the user using a Toast.
+  var data = {
+    message: 'You must sign-in first',
+    timeout: 2000
+  };
+  this.signInSnackbar.MaterialSnackbar.showSnackbar(data);
+  return false;
+};
+
+// Requests permissions to show notifications.
+mainPesquisas.prototype.requestNotificationsPermissions = function() {
+  console.log('Requesting notifications permission...');
+  firebase.messaging().requestPermission().then(function() {
+    // Notification permission granted.
+    this.saveMessagingDeviceToken();
+  }.bind(this)).catch(function(error) {
+    console.error('Unable to get permission to notify.', error);
+  });
+};
+
+// Enables or disables the submit button depending on the values of the input
+// fields.
+mainPesquisas.prototype.toggleButton = function() {
+  if (this.messageInput.value) {
+    this.submitButton.removeAttribute('disabled');
+  } else {
+    this.submitButton.setAttribute('disabled', 'true');
+  }
+};
+
+// Checks that the Firebase SDK has been correctly setup and configured.
+mainPesquisas.prototype.checkSetup = function() {
+  if (!window.firebase || !(firebase.app instanceof Function) || !firebase.app().options) {
+    window.alert('You have not configured and imported the Firebase SDK. ' +
+        'Make sure you go through the codelab setup instructions and make ' +
+        'sure you are running the codelab using `firebase serve`');
+  }
+};
+
+window.onload = function() {
+  window.mainPesquisas = new mainPesquisas();
+};
+
+// Saves the messaging device token to the datastore.
+mainPesquisas.prototype.saveMessagingDeviceToken = function() {
+  firebase.messaging().getToken().then(function(currentToken) {
+    if (currentToken) {
+      console.log('Got FCM device token:', currentToken);
+      // Saving the Device Token to the datastore.
+      firebase.database().ref('/fcmTokens').child(currentToken)
+          .set(firebase.auth().currentUser.uid);
+    } else {
+      // Need to request permissions to show notifications.
+      this.requestNotificationsPermissions();
+    }
+  }.bind(this)).catch(function(error){
+    console.error('Unable to get messaging token.', error);
+  });
+};
+
 // Saves a new message on the Firebase DB.
-FriendlyChat.prototype.saveMessage = function(e) {
+mainPesquisas.prototype.saveMessage = function(e) {
   e.preventDefault();
   // Check that the user entered a message and is signed in.
   if (this.messageInput.value && this.checkSignedInWithMessage()) {
@@ -78,155 +208,67 @@ FriendlyChat.prototype.saveMessage = function(e) {
 	  dataCriacao:"01/11/2017"
     }).then(function() {
       // Clear message text field and SEND button state.
-      FriendlyChat.resetMaterialTextfield(this.messageInput);
-	  FriendlyChat.resetMaterialTextfield(this.opcaoInput);
       this.toggleButton();
     }.bind(this)).catch(function(error) {
       console.error('Error writing new message to Firebase Database', error);
     });
   }
-};
-
-// Sets up shortcuts to Firebase features and initiate firebase auth.
-FriendlyChat.prototype.initFirebase = function() {
-  // Shortcuts to Firebase SDK features.
-  this.auth = firebase.auth();
-  this.database = firebase.database();
-  this.storage = firebase.storage();
-  // Initiates Firebase auth and listen to auth state changes.
-  this.auth.onAuthStateChanged(this.onAuthStateChanged.bind(this));
+  document.getElementById('pergunta').value=''; // Limpa o campo
+  document.getElementById('opcoes').value=''; // Limpa o campo
 };
 
 // Loads chat messages history and listens for upcoming ones.
-FriendlyChat.prototype.loadPesquisas = function() {
+mainPesquisas.prototype.loadPesquisas = function() {
   // Reference to the /messages/ database path.
   this.messagesRef = this.database.ref('publik/pesquisas');
   // Make sure we remove all previous listeners.
   this.messagesRef.off();
+  // Loads the last 12 messages and listen for new ones.
+  var setMessage = function(data) {
+    var val = data.val();
+    this.displayPesquisas(data.key, val.pergunta, val.status, val.dataCriacao);
+  }.bind(this);
+
+  this.messagesRef.on('child_added', setMessage);
+  this.messagesRef.on('child_changed', setMessage);
+
 };
 
-// Sets the URL of the given img element with the URL of the image stored in Cloud Storage.
-FriendlyChat.prototype.setImageUrl = function(imageUri, imgElement) {
-  // If the image is a Cloud Storage URI we fetch the URL.
-  if (imageUri.startsWith('gs://')) {
-    imgElement.src = FriendlyChat.LOADING_IMAGE_URL; // Display a loading image first.
-    this.storage.refFromURL(imageUri).getMetadata().then(function(metadata) {
-      imgElement.src = metadata.downloadURLs[0];
-    });
-  } else {
-    imgElement.src = imageUri;
-  }
+// Displays a Message in the UI.
+mainPesquisas.prototype.displayPesquisas = function(key, pergunta, status, dataCriacao) {   
+	var div = document.getElementById(key);
+	
+	// If an element for that message does not exists yet we create it.
+	if (!div) {
+	var container = document.createElement('div');	
+	container.innerHTML = '<div class="card-header bg-white">'+
+	'	<div class="message-container">' +
+	'	<div class="spacing"><div class="pic"></div></div>' +
+	'	<form >'+
+	'		<h5><i class="pergunta fa fa-lg fa-question-circle"></i></h5>'+
+	'		<i><h6 class="dataCriacao text-muted"></h6></i>'+
+	'		<i><h6 class="status text-muted"></h6></i>'+
+	'		<a class="btn btn-dark" href="detalhesPesquisa.html?id='+key+'"> Ver Detalhes </a>'+
+	'	</form>'+
+	'</div>';
+
+	div = container.firstChild;
+	div.setAttribute('id', key);
+	this.messageList.appendChild(div);
+	}
+
+	div.querySelector('.pergunta').textContent = " "+pergunta;
+	var respostasElement = div.querySelector('.dataCriacao');
+	var escolhasElement = div.querySelector('.status');
+
+	respostasElement.textContent = "Criado em "+dataCriacao;
+	escolhasElement.textContent = "Satatus: "+status;
+
+	respostasElement.innerHTML = respostasElement.innerHTML.replace(/\n/g, '<br>');
+
+	// Show the card fading-in and scroll to view the new message.
+	setTimeout(function() {div.classList.add('visible')}, 1);
+
+	this.messageList.scrollTop = this.messageList.scrollHeight;
 };
 
-// Signs-in Friendly Chat.
-FriendlyChat.prototype.signIn = function() {
-  // Sign in Firebase using popup auth and Google as the identity provider.
-  var provider = new firebase.auth.GoogleAuthProvider();
-  this.auth.signInWithPopup(provider);
-};
-
-// Signs-out of Friendly Chat.
-FriendlyChat.prototype.signOut = function() {
-  // Sign out of Firebase.
-  this.auth.signOut();
-};
-
-// Triggers when the auth state change for instance when the user signs-in or signs-out.
-FriendlyChat.prototype.onAuthStateChanged = function(user) {
-  if (user) { // User is signed in!
-    // Get profile pic and user's name from the Firebase user object.
-    var profilePicUrl = user.photoURL;
-    var userName = user.displayName;
-
-    // Show user's profile and sign-out button.
-    this.signOutButton.removeAttribute('hidden');
-
-    // Hide sign-in button.
-    this.signInButton.setAttribute('hidden', 'true');
-
-    // We load currently existing chant messages.
-    this.loadPesquisas();
-
-    // We save the Firebase Messaging Device token and enable notifications.
-    this.saveMessagingDeviceToken();
-  } else { // User is signed out!
-    // Hide user's profile and sign-out button.
-    this.signOutButton.setAttribute('hidden', 'true');
-
-    // Show sign-in button.
-    this.signInButton.removeAttribute('hidden');
-  }
-};
-
-// Returns true if user is signed-in. Otherwise false and displays a message.
-FriendlyChat.prototype.checkSignedInWithMessage = function() {
-  // Return true if the user is signed in Firebase
-  if (this.auth.currentUser) {
-    return true;
-  }
-
-  // Display a message to the user using a Toast.
-  var data = {
-    message: 'You must sign-in first',
-    timeout: 2000
-  };
-  this.signInSnackbar.MaterialSnackbar.showSnackbar(data);
-  return false;
-};
-
-// Saves the messaging device token to the datastore.
-FriendlyChat.prototype.saveMessagingDeviceToken = function() {
-  firebase.messaging().getToken().then(function(currentToken) {
-    if (currentToken) {
-      console.log('Got FCM device token:', currentToken);
-      // Saving the Device Token to the datastore.
-      firebase.database().ref('/fcmTokens').child(currentToken)
-          .set(firebase.auth().currentUser.uid);
-    } else {
-      // Need to request permissions to show notifications.
-      this.requestNotificationsPermissions();
-    }
-  }.bind(this)).catch(function(error){
-    console.error('Unable to get messaging token.', error);
-  });
-};
-
-// Requests permissions to show notifications.
-FriendlyChat.prototype.requestNotificationsPermissions = function() {
-  console.log('Requesting notifications permission...');
-  firebase.messaging().requestPermission().then(function() {
-    // Notification permission granted.
-    this.saveMessagingDeviceToken();
-  }.bind(this)).catch(function(error) {
-    console.error('Unable to get permission to notify.', error);
-  });
-};
-
-// Resets the given MaterialTextField.
-FriendlyChat.resetMaterialTextfield = function(element) {
-  element.value = '';
-  element.parentNode.MaterialTextfield.boundUpdateClassesHandler();
-};
-
-// Enables or disables the submit button depending on the values of the input
-// fields.
-FriendlyChat.prototype.toggleButton = function() {
-  if (this.messageInput.value) {
-    this.submitButton.removeAttribute('disabled');
-  } else {
-    this.submitButton.setAttribute('disabled', 'true');
-  }
-};
-
-// Checks that the Firebase SDK has been correctly setup and configured.
-FriendlyChat.prototype.checkSetup = function() {
-  if (!window.firebase || !(firebase.app instanceof Function) || !firebase.app().options) {
-    window.alert('You have not configured and imported the Firebase SDK. ' +
-        'Make sure you go through the codelab setup instructions and make ' +
-        'sure you are running the codelab using `firebase serve`');
-  }
-};
-
-window.onload = function() {
-  window.friendlyChat = new FriendlyChat();
-};
--- scripts/mainDetalhes.js
+++ scripts/mainDetalhes.js
@@ -15,8 +15,17 @@
  */
 'use strict';
 
-// Initializes FriendlyChat.
-function FriendlyChat() {
+var geocoder;
+var infoWindow;
+var map;
+var marker;
+
+window.onload = function() {
+  window.mainDetalhes = new mainDetalhes();
+};
+
+// Initializes mainDetalhes.
+function mainDetalhes() {
   this.checkSetup();
 
   // Shortcuts to DOM Elements.
@@ -33,7 +42,7 @@ function FriendlyChat() {
 }
 
 // Sets up shortcuts to Firebase features and initiate firebase auth.
-FriendlyChat.prototype.initFirebase = function() {
+mainDetalhes.prototype.initFirebase = function() {
   // Shortcuts to Firebase SDK features.
   this.auth = firebase.auth();
   this.database = firebase.database();
@@ -42,38 +51,21 @@ FriendlyChat.prototype.initFirebase = function() {
   this.auth.onAuthStateChanged(this.onAuthStateChanged.bind(this));
 };
 
-// Loads chat messages history and listens for upcoming ones.
-FriendlyChat.prototype.loadMessages = function() {
-  // Reference to the /messages/ database path.
-  this.messagesRef = this.database.ref('publik/locais');
-  // Make sure we remove all previous listeners.
-  this.messagesRef.off();
-
-  // Loads the last 12 messages and listen for new ones.
-  var setMessage = function(data) {
-    var val = data.val();
-    this.displayMessage(data.key, val.nome, val.endereco, val.avaliacao, val.historia, val.taxas);  
-  }.bind(this);
-  
-  this.messagesRef.limitToLast(12).on('child_added', setMessage);
-  this.messagesRef.limitToLast(12).on('child_changed', setMessage);
-};
-
 // Signs-in Friendly Chat.
-FriendlyChat.prototype.signIn = function() {
+mainDetalhes.prototype.signIn = function() {
   // Sign in Firebase using popup auth and Google as the identity provider.
   var provider = new firebase.auth.GoogleAuthProvider();
   this.auth.signInWithPopup(provider);
 };
 
 // Signs-out of Friendly Chat.
-FriendlyChat.prototype.signOut = function() {
+mainDetalhes.prototype.signOut = function() {
   // Sign out of Firebase.
   this.auth.signOut();
 };
 
 // Triggers when the auth state change for instance when the user signs-in or signs-out.
-FriendlyChat.prototype.onAuthStateChanged = function(user) {
+mainDetalhes.prototype.onAuthStateChanged = function(user) {
   if (user) { // User is signed in!
     // Show user's profile and sign-out button.
     this.signOutButton.removeAttribute('hidden');
@@ -96,7 +88,7 @@ FriendlyChat.prototype.onAuthStateChanged = function(user) {
 };
 
 // Returns true if user is signed-in. Otherwise false and displays a message.
-FriendlyChat.prototype.checkSignedInWithMessage = function() {
+mainDetalhes.prototype.checkSignedInWithMessage = function() {
   // Return true if the user is signed in Firebase
   if (this.auth.currentUser) {
     return true;
@@ -112,7 +104,7 @@ FriendlyChat.prototype.checkSignedInWithMessage = function() {
 };
 
 // Saves the messaging device token to the datastore.
-FriendlyChat.prototype.saveMessagingDeviceToken = function() {
+mainDetalhes.prototype.saveMessagingDeviceToken = function() {
   firebase.messaging().getToken().then(function(currentToken) {
     if (currentToken) {
       console.log('Got FCM device token:', currentToken);
@@ -129,7 +121,7 @@ FriendlyChat.prototype.saveMessagingDeviceToken = function() {
 };
 
 // Requests permissions to show notifications.
-FriendlyChat.prototype.requestNotificationsPermissions = function() {
+mainDetalhes.prototype.requestNotificationsPermissions = function() {
   console.log('Requesting notifications permission...');
   firebase.messaging().requestPermission().then(function() {
     // Notification permission granted.
@@ -139,14 +131,34 @@ FriendlyChat.prototype.requestNotificationsPermissions = function() {
   });
 };
 
-// Resets the given MaterialTextField.
-FriendlyChat.resetMaterialTextfield = function(element) {
-  element.value = '';
-  element.parentNode.MaterialTextfield.boundUpdateClassesHandler();
+// Checks that the Firebase SDK has been correctly setup and configured.
+mainDetalhes.prototype.checkSetup = function() {
+  if (!window.firebase || !(firebase.app instanceof Function) || !firebase.app().options) {
+    window.alert('You have not configured and imported the Firebase SDK. ' +
+        'Make sure you go through the codelab setup instructions and make ' +
+        'sure you are running the codelab using `firebase serve`');
+  }
+};
+
+// Loads chat messages history and listens for upcoming ones.
+mainDetalhes.prototype.loadMessages = function() {
+  // Reference to the /messages/ database path.
+  this.messagesRef = this.database.ref('publik/locais');
+  // Make sure we remove all previous listeners.
+  this.messagesRef.off();
+
+  // Loads the last 12 messages and listen for new ones.
+  var setMessage = function(data) {
+    var val = data.val();
+    this.displayMessage(data.key, val.nome, val.endereco, val.avaliacao, val.historia, val.taxas);  
+  }.bind(this);
+  
+  this.messagesRef.limitToLast(12).on('child_added', setMessage);
+  this.messagesRef.limitToLast(12).on('child_changed', setMessage);
 };
 
 // Displays a Message in the UI.
-FriendlyChat.prototype.displayMessage = function(key, nome, endereco, avaliacao, historia, taxa) {  
+mainDetalhes.prototype.displayMessage = function(key, nome, endereco, avaliacao, historia, taxa) {  
   var txtNome = document.getElementById("nome");
   txtNome.textContent = nome;
   
@@ -176,7 +188,7 @@ FriendlyChat.prototype.displayMessage = function(key, nome, endereco, avaliacao,
   this.novosUsersChart(taxa.mediaMensal);
 };
 
-FriendlyChat.prototype.nivelSatisChart = function(nivel){
+mainDetalhes.prototype.nivelSatisChart = function(nivel){
 	new Chartist.Bar('#chartSatisfacao', {
 	  series: [[nivel]]
 	}, {
@@ -191,7 +203,7 @@ FriendlyChat.prototype.nivelSatisChart = function(nivel){
 	});
 };
 
-FriendlyChat.prototype.classeSocialChart = function(a,b,c,d,e){
+mainDetalhes.prototype.classeSocialChart = function(a,b,c,d,e){
 	var labels = ['A', 'B', 'C', 'D', 'E'];
 	
 	var data = {
@@ -222,7 +234,7 @@ FriendlyChat.prototype.classeSocialChart = function(a,b,c,d,e){
 	new Chartist.Pie('#chartClasseSocial', data, options, responsiveOptions);
 };
 
-FriendlyChat.prototype.faixaEtariaChart = function(a,b,c,d,e,f){
+mainDetalhes.prototype.faixaEtariaChart = function(a,b,c,d,e,f){
 	var labels = ['0 a 9', '10 a 19', '20 a 29', '30 a 39', '40 a 49', 'a partir de 50'];
 	
 	var data = {
@@ -253,7 +265,7 @@ FriendlyChat.prototype.faixaEtariaChart = function(a,b,c,d,e,f){
 	new Chartist.Pie('#chartFaixaEtaria', data, options, responsiveOptions);
 };
 
-FriendlyChat.prototype.plataformaChart = function(android, ios, outros){
+mainDetalhes.prototype.plataformaChart = function(android, ios, outros){
 	var labels = ['Android', 'IOs', 'Outros'];
 	
 	var data = {
@@ -284,7 +296,7 @@ FriendlyChat.prototype.plataformaChart = function(android, ios, outros){
 	new Chartist.Pie('#chartPlataformas', data, options, responsiveOptions);
 };
 
-FriendlyChat.prototype.freqMensalChart = function(jan, fev, mar, abr, mai, jun, jul, ago, set, out, nov, dez){
+mainDetalhes.prototype.freqMensalChart = function(jan, fev, mar, abr, mai, jun, jul, ago, set, out, nov, dez){
 	var data = {
 	  labels: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'],
 	  series: [[jan, fev, mar, abr, mai, jun, jul, ago, set, out, nov, dez]]
@@ -306,7 +318,7 @@ FriendlyChat.prototype.freqMensalChart = function(jan, fev, mar, abr, mai, jun, 
 	new Chartist.Bar('#chartFreqMensal', data, options, responsiveOptions);
 };
 
-FriendlyChat.prototype.freqDiariaChart = function(dia1,dia2,dia3,dia4,dia5,dia6,dia7,dia8,dia9,dia10,dia11,dia12,dia13,dia14,dia15,dia16,dia17,dia18,dia19,dia20,dia21,dia22,dia23,dia24,dia25,dia26,dia27,dia28,dia29,dia30,dia31){
+mainDetalhes.prototype.freqDiariaChart = function(dia1,dia2,dia3,dia4,dia5,dia6,dia7,dia8,dia9,dia10,dia11,dia12,dia13,dia14,dia15,dia16,dia17,dia18,dia19,dia20,dia21,dia22,dia23,dia24,dia25,dia26,dia27,dia28,dia29,dia30,dia31){
 	var data = {
 	  labels: ["01","02","03","04","05","06","07","08","09","10","11","12","13","14","15","16","17","18","19","20","21","22","23","24","25","26","27","28","29","30","31"],
 	  series: [[dia1,dia2,dia3,dia4,dia5,dia6,dia7,dia8,dia9,dia10,dia11,dia12,dia13,dia14,dia15,dia16,dia17,dia18,dia19,dia20,dia21,dia22,dia23,dia24,dia25,dia26,dia27,dia28,dia29,dia30,dia31]]
@@ -329,7 +341,7 @@ FriendlyChat.prototype.freqDiariaChart = function(dia1,dia2,dia3,dia4,dia5,dia6,
 	new Chartist.Bar('#chartFreqDiaria', data, options, responsiveOptions);
 };
 
-FriendlyChat.prototype.novosUsersChart = function(mediaMensal){
+mainDetalhes.prototype.novosUsersChart = function(mediaMensal){
 	new Chartist.Pie(
 		'#chartNovosUsuariosMensal', 
 		{
@@ -344,15 +356,5 @@ FriendlyChat.prototype.novosUsersChart = function(mediaMensal){
 		}
 	);
 };
-// Checks that the Firebase SDK has been correctly setup and configured.
-FriendlyChat.prototype.checkSetup = function() {
-  if (!window.firebase || !(firebase.app instanceof Function) || !firebase.app().options) {
-    window.alert('You have not configured and imported the Firebase SDK. ' +
-        'Make sure you go through the codelab setup instructions and make ' +
-        'sure you are running the codelab using `firebase serve`');
-  }
-};
 
-window.onload = function() {
-  window.friendlyChat = new FriendlyChat();
-};
+
